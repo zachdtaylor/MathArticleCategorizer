9efbdd6f713b937ef8fe1d202fd4a247438ee882
mattoehler <mjoehler94@gmail.com>
2017-09-22 13:50:45 -0600

    updated SVM.py

--- SVM.py
+++ SVM.py
@@ -20,8 +20,17 @@ def train_svm(X, y):
 #Main--------------------------------------------------------------
 
 
+def runSVC(X,y):
+    '''
+    Parameters
+    ----------
+    X = All of the clusters that we have for both Math and non-Math articles
+    y = Whether or not  the articles associated with the clusters are 'high-quality'
+
+    
+    '''
 #split up train/test data
-X_train, X_test, y_train, y_test = train_test_split(counts, df.popularity, test_size=0.25, random_state=10)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25, random_state=10)
 
 
 #fit the model
@@ -30,9 +39,10 @@ svm = train_svm(X_train, y_train)
 train_end = time.time()
 print('Training time:', train_end-train_start)
  
-
-accuracy = svm.score(X_test, y_test)
-print('Accuracy:',accuracy)
+benchmark = 
+score = svm.score(X_test, y_test)
+print('Benchmark: )
+print('Score:',score)
 
 
 
