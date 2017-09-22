b5000a705728acc45e290339325b00774cc63c27
Kaden Barlow <kadenbarlow@gmail.com>
2017-09-22 13:18:56 -0600

    added kmeans back

--- DocumentVector.py
+++ DocumentVector.py
@@ -8,6 +8,8 @@ import xml.etree.ElementTree as ET
 from lxml import etree
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import MeanShift
+from sklearn.cluster import KMeans
+
 
 
 #Helper Functions--------------------------------------------------------------
@@ -40,9 +42,10 @@ def get_clusters(doc_number, doc_word_vectors, text_list, model):
     words = [word for word in clean_text_list(text_list[doc_number]) if word in model.wv]
     word_vecs = {tuple(key): value for (key, value) in zip(document, words)}
 
-    #dbscan = DBSCAN(eps=.000000001, metric='cosine', algorithm='brute', min_samples=1)
-    ms = MeanShift()
-    clusters = ms.fit(doc_word_vectors[doc_number])
+    cls_alg = KMeans(n_clusters=50)
+    #cls_alg = DBSCAN(eps=.000000001, metric='cosine', algorithm='brute', min_samples=1)
+    #cls_alg = MeanShift()
+    clusters = cls_alg.fit(doc_word_vectors[doc_number])
 
     vector_clusters = defaultdict(set)
     for i in range(len(clusters.labels_)):
@@ -54,6 +57,7 @@ def get_clusters(doc_number, doc_word_vectors, text_list, model):
         for vector in vector_clusters[cluster]:
             print(word_vecs[vector])
         print("\n")
+
 
     return vector_clusters
 
