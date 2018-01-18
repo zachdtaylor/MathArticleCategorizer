5feaa58bb3cea487990580c25a55bb68929982b0
zacharytaylor <zachtylr04@gmail.com>
2018-01-18 15:57:42 -0700

    Add new DV file

--- DV.py
+++ DV.py
@@ -1,18 +1,11 @@
-#Imports-----------------------------------------------------------------------
-import pandas as pd
-import string
-from collections import defaultdict
 import gensim
 import numpy as np
-import xml.etree.ElementTree as ET
-from lxml import etree
+import string
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import MeanShift
 from sklearn.cluster import KMeans
-import json
-import sys
+from collections import defaultdict
 
-#Dictionary for TF-IDF scores--------------------------------------------------
 idf_dict = dict()
 with open("frequentwords.txt", 'r') as f:
     for line in f:
@@ -20,21 +13,6 @@ with open("frequentwords.txt", 'r') as f:
         idf_dict[word] = int(freq)
 
 total_words = sum(idf_dict.values())
-
-#Helper Functions--------------------------------------------------------------
-def xml2df(xml_data):
-    tree = ET.parse(xml_data)
-    root = tree.getroot()
-    all_records = []
-    headers = []
-    for i, child in enumerate(root):
-        record = []
-        for subchild in child:
-            record.append(subchild.text)
-            if subchild.tag not in headers:
-                headers.append(subchild.tag)
-        all_records.append(record)
-    return pd.DataFrame(all_records, columns=headers)
 
 def clean_word(w):
     strip_str = "()\".?!,;"
@@ -45,6 +23,12 @@ def clean_text_list(doc):
     words = doc.split()
     clean_words = [clean_word(word) for word in words]
     return clean_words
+
+def doc2vec(document, model):
+    words = np.array([word for word in clean_text_list(document) if word in model.wv])
+    vectors = np.array([model.wv[word] for word in words])
+    cluster = get_cluster(vectors, words, model)
+    return np.sum(np.asarray(list(vecs)), axis=0) / len(vecs)
 
 def get_cluster(vectors, words, model):
     """
@@ -110,41 +94,3 @@ def tf_idf(cluster, words):
         idf = idf_dict[word] / total_words if word in idf_dict else 1
         score += tf * idf
     return score
-
-# Main Function -------------------------------------------
-def getVectors(file_name):
-    print("Starting Script...")
-    print("GoodArticles Created.")
-    model = gensim.models.KeyedVectors.load_word2vec_format('../GoogleNews-vectors-negative300.bin', binary=True)
-    print("Google words model loaded.")
-    text = xml2df(file_name)["Text"].as_matrix()
-
-    # docs_words contains each document as an ndarray of words
-    docs_words = [np.array([word for word in clean_text_list(doc) if word in model.wv]) for doc in text if type(doc) == str]
-    # docs_vectors contains each document as an array of word vectors
-    docs_vectors = [np.array([model.wv[word] for word in doc]) for doc in docs_words]
-
-    document_vectors = []
-    print(len(docs_words))
-    print(len(docs_vectors))
-    print("Beginning loop...")
-    print("Number of documents: ", len(docs_words))
-    for k in range(len(docs_words)):
-        print("doc # ", k)
-        vecs = get_cluster(docs_vectors[k],docs_words[k],model)
-        doc_vec = np.sum(np.asarray(list(vecs)), axis=0) / len(vecs)
-        document_vectors.append(doc_vec)
-    
-    print("Saving to doc_vectors.npy...")
-    np.save("doc_vectors.npy", np.array(document_vectors))
-    print("Done.")
-
-    # with open("DataSetX.txt", 'w') as f:
-    #     f.write(json.dumps(clusters))
-
-    # with open("DataSetY.txt", 'w') as f:
-    #     f.write(json.dumps(ratings))
-
-#Run Above Functions
-if __name__ == "__main__":
-    getVectors(sys.argv[1])
