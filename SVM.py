9224e6d4d3114da77ad57d116ac375a2303a8e6d
mattoehler <mjoehler94@gmail.com>
2018-01-24 14:52:57 -0700

    adding pipeline and pickeled MF good articles

--- SVM.py
+++ SVM.py
@@ -7,7 +7,8 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 import time
 import json
-
+import numpy as np
+import pandas as pd
 
 #Helper Functions--------------------------------------------------
     
@@ -15,26 +16,31 @@ def train_model(X,y,model):
         model.fit(X,y)
         return model   
 
-def runModel(xfile,yfile,model,save_model = False):
+def runModel(data_file,model,save_model = False):
     '''
     Parameters
     ----------
     xfile = (string) name of file of all clusters representations that we have for both Math and non-Math articles
     y = (string) name of file indicating the rating of the article (0=bad, 1=good) 
-    model = the type of model that you want to run [ie: for Naive Bayes => MultinomialNB()]
+    model = the type of model that you want to run [eg: for Naive Bayes => MultinomialNB()]
     '''
     
-#    with open('DataSetX.txt','r') as f:
+#    #we aren't using  json anymore
+#    with open(xfile,'r') as f:
 #        X = json.loads(f.read())
 #    
-#    with open('DataSetY.txt','r') as f:
-#        y = json.loads(f.read())
+#    with open(yfile,'r') as f:
+#        y = json.loads(f.read())   
+
+    # data = np.load (data_file)#this should load the file like a numpy array
+    # X = data[0] #X and y need to be updated to properly index from the data file
+    # y = data[0]
     
-    with open(xfile,'r') as f:
-        X = json.loads(f.read())
     
-    with open(yfile,'r') as f:
-        y = json.loads(f.read())    
+    #read in the data file
+    data = pd.read_csv(data_file)
+    X = data[data.columns[1]]
+    y = data[data.columns[0]]
     
     #split up train/test data
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25, random_state=10)
@@ -67,11 +73,10 @@ model = SVC()
 
 print("Begin Script")
 #sys.argv[0] is the name of the script
-xfile = sys.argv[1]
-yfile = sys.argv[2]
-save  = sys.argv[3]
+data_file = sys.argv[1]
+save      = sys.argv[2]
 
-runModel(xfile,yfile,model,save)
+runModel(data_file,model,save)
 
 print("Training Complete")
 
